1d38855e95b464fb9e13d6f44d234bacd578a42f
lojhanINDT <Vinicius.Hernandes@indt.org.br>
2021-09-23 21:57:23 -0300

    minor cors fixes

--- Startup.cs
+++ Startup.cs
@@ -81,17 +81,19 @@ namespace proj_semestre_backend
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "proj_semestre_backend v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
+    
 
             app.UseCors(x => x
                 .AllowAnyOrigin()
                 .AllowAnyMethod()
                 .AllowAnyHeader());
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
